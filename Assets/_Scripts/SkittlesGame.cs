92be8fd4bb71541fa3ae9cf03e4f313371691bc6
mtsandmeyer <32112441+mtsandmeyer@users.noreply.github.com>
2018-03-26 10:30:35 -0400

    Initial Scenes
    Split scenes between facilitator and user and figured out a way to get multi display.

---
+++ Assets/_Scripts/SkittlesGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkittlesGame : MonoBehaviour {
+
+    // The state of the game
+    // Pretrial - Ball not yet thrown
+    // Swinging - Ball was thrown, is currently swinging on its trajectory
+    // Posttrial - Bass has completed its trajectory. Feedback is given to user
+	public enum GameState
+    {PRE_TRIAL, SWINGING, POST_TRIAL};
+
+    // The current game state
+    private GameState curGameState = GameState.PRE_TRIAL;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
